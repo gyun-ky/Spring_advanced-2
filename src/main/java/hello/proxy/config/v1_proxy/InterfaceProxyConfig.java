e1a472042e69fd486257fd4d53f0987f9a55b3c4
gyun_ky <kiyoon9390@gmail.com>
2024-01-15 22:53:56 +0900

    feat : proxy config

---
+++ src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
@@ -0,0 +1,37 @@
+package hello.proxy.config.v1_proxy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.proxy.app.v1.OrderControllerV1;
+import hello.proxy.app.v1.OrderControllerV1Impl;
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.app.v1.OrderRepositoryV1Impl;
+import hello.proxy.app.v1.OrderServiceV1;
+import hello.proxy.app.v1.OrderServiceV1Impl;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
+import hello.proxy.trace.logtrace.LogTrace;
+
+@Configuration
+public class InterfaceProxyConfig {
+
+	@Bean
+	public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
+		OrderControllerV1 orderControllerV1 = new OrderControllerV1Impl(orderServiceV1(logTrace));
+		return new OrderControllerInterfaceProxy(orderControllerV1, logTrace);
+	}
+
+	@Bean
+	public OrderServiceV1 orderServiceV1(LogTrace logTrace) {
+		OrderServiceV1 orderServiceV1 = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
+		return new OrderServiceInterfaceProxy(orderServiceV1, logTrace);
+	}
+
+	@Bean
+	public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace) {
+		OrderRepositoryV1 orderRepositoryV1 = new OrderRepositoryV1Impl();
+		return new OrderRepositoryInterfaceProxy(orderRepositoryV1, logTrace);
+	}
+}
